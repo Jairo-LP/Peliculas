340f88d044f1a723a78b6bb27cf580d1ecf64840
Jairo <jairo15102001@gmail.com>
2026-03-31 08:03:52 -0500

    Modificado Clase

--- hellogit.java
+++ hellogit.java
@@ -3,4 +3,11 @@ import java.util.Scanner;
 void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.printf("Hola mundo Tengo algo nuevo");
+    System.out.println("IMPLEMENTACION 31 -3-26");
+    
+    System.out.println("Ingrese el nombre de la mascota: ");
+     String nombre = sc.nextLine();
+    
+     System.out.println("El nombre de la mascota es: "+nombre);
+     
 }
