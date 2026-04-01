1e0b5a82fca1eeb6a9da59a6aa2d163e5352575a
Jairo <jairo15102001@gmail.com>
2026-03-31 21:42:41 -0500

    PrimerosVN

---
+++ class main1.java
@@ -0,0 +1,15 @@
+class main1.java
+import java.util.Scanner;
+public class main {
+void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.printf("Hola mundo Tengo algo nuevo");
+    System.out.println("IMPLEMENTACION 31 -3-26");
+    
+    System.out.println("Ingrese el nombre de la mascota: ");
+     String nombre = sc.nextLine();
+    
+     System.out.println("El nombre de la mascota es: "+nombre);
+
+}   
+}
